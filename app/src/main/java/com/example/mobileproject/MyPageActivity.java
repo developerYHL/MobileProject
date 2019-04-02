dd50eb15f62afb13ab6f55c85d1cea4d73d851db
csh <tizkzks96@gamil.com>
2019-04-02 20:10:28 +0900

    update Java,layout

--- app/src/main/java/com/example/mobileproject/MyPageActivity.java
+++ app/src/main/java/com/example/mobileproject/MyPageActivity.java
@@ -7,20 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MyPageActivity extends AppCompatActivity {
     Context context;
     Button goMainBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_my_page);
         context = this;
         goMainBtn = findViewById(R.id.button_Main);
 
         goMainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, MyPageActivity.class);
+                Intent intent = new Intent(context, MainActivity.class);
                 startActivity(intent);
             }
         });
