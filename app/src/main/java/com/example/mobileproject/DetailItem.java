e7adc374208376b185833399ed12fe18a11b7b90
csh <tizkzks96@naver.com>
2019-04-03 11:26:49 +0900

    add recyclerview

---
+++ app/src/main/java/com/example/mobileproject/DetailItem.java
@@ -0,0 +1,36 @@
+package com.example.mobileproject;
+
+public class DetailItem {
+    private String title;
+    private String contents;
+
+    public DetailItem(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("CardItem{");
+        sb.append("title='").append(title).append('\'');
+        sb.append(", contents='").append(contents).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
--- app/src/main/java/com/example/mobileproject/MainActivity.java
+++ app/src/main/java/com/example/mobileproject/MainActivity.java
@@ -1,29 +1,79 @@
 package com.example.mobileproject;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-    Context context;
-    Button goMainBtn;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements RecyclerAdapter.MyRecyclerViewClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private RecyclerAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context = this;
-        goMainBtn = findViewById(R.id.button_Main);
 
-        goMainBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, MyPageActivity.class);
-                startActivity(intent);
-            }
-        });
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
+        recyclerView.setHasFixedSize(false);
+
+        // 레이아웃 매니저로 LinearLayoutManager를 설정
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+//        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // 표시할 임시 데이터
+        List<DetailItem> dataList = new ArrayList<>();
+        dataList.add(new DetailItem("이것은 첫번째 아이템", "안드로이드 보이라고 합니다"));
+        dataList.add(new DetailItem("이것은 세번째 아이템", "이번엔 세줄\n두번째 줄\n세번째 줄 입니다"));
+        dataList.add(new DetailItem("이것은 두번째 아이템", "두 줄 입력도 해 볼게요\n두 줄 입니다"));
+        dataList.add(new DetailItem("이것은 네번째 아이템", "잘 되네요"));
+
+        // 어댑터 설정
+        mAdapter = new RecyclerAdapter(dataList);
+        mAdapter.setOnClickListener(this);
+        recyclerView.setAdapter(mAdapter);
+
+        // ItemAnimator
+        DefaultItemAnimator animator = new DefaultItemAnimator();
+        animator.setAddDuration(1000);
+        animator.setRemoveDuration(1000);
+        animator.setMoveDuration(1000);
+        animator.setChangeDuration(1000);
+        recyclerView.setItemAnimator(animator);
+
+        // ItemDecoration
+        DividerItemDecoration decoration = new DividerItemDecoration(this, layoutManager.getOrientation());
+        recyclerView.addItemDecoration(decoration);
+    }
+
+    @Override
+    public void onItemClicked(int position) {
+        Log.d(TAG, "onItemClicked: " + position);
+    }
+
+    @Override
+    public void onShareButtonClicked(int position) {
+        Log.d(TAG, "onShareButtonClicked: " + position);
+
+        mAdapter.addItem(position, new DetailItem("추가 됨", "추가 됨"));
+    }
+
+    @Override
+    public void onLearnMoreButtonClicked(int position) {
+        Log.d(TAG, "onLearnMoreButtonClicked: " + position);
+
+        // 아이템 삭제
+        mAdapter.removeItem(position);
     }
 }
