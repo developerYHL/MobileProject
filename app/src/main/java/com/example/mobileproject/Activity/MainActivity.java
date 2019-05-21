4ba7302a104f5ea2bcf25566b21f4402ce6e001c
csh <tizkzks96@naver.com>
2019-05-21 18:26:38 +0900

    postActivity 재 추가

--- app/src/main/java/com/example/mobileproject/Activity/MainActivity.java
+++ app/src/main/java/com/example/mobileproject/Activity/MainActivity.java
@@ -86,20 +86,6 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-
-        }
-    }
 
 }
--- app/src/main/java/com/example/mobileproject/Activity/PostActivity.java
+++ app/src/main/java/com/example/mobileproject/Activity/PostActivity.java
@@ -1,4 +1,13 @@
 package com.example.mobileproject.Activity;
 
-public class PostActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
 }
