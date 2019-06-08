a17cda40e7b03d95f25f28a1f7f6037af999b0c9
developerYHL <developer.yhl@gmail.com>
2019-06-09 05:29:30 +0900

    Google Map에 Glide를 이용해 ImageView에 url로 image넣기 or drawable image넣기 완성 // Clustering ImageView로 바꿀 예정

--- app/src/main/java/com/example/mobileproject/fragments/NavigationFragment.java
+++ app/src/main/java/com/example/mobileproject/fragments/NavigationFragment.java
@@ -1,8 +1,15 @@
 package com.example.mobileproject.fragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,14 +26,19 @@ import android.view.ViewGroup;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -40,14 +52,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.maps.android.clustering.ClusterManager;
 
 import android.support.v4.app.FragmentManager;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.support.v4.content.ContextCompat.getSystemService;
+
 public class NavigationFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    private  static  final int REQUEST_CODE_PERMISSIONS = 1000;
+    /*private  static  final int REQUEST_CODE_PERMISSIONS = 1000;
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient;
 
@@ -90,10 +105,10 @@ public class NavigationFragment extends Fragment implements OnMapReadyCallback, 
         mMap = googleMap;
 
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(17.0f));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(51.86, 0.22)));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(7.0f));
 
         mMap.setOnInfoWindowClickListener((marker) -> {
             Intent intent = new Intent((Intent.ACTION_DIAL));
@@ -115,6 +130,109 @@ public class NavigationFragment extends Fragment implements OnMapReadyCallback, 
             }
         });
         addItems();
+    }*/
+
+    // region Constants
+
+    private static final String TAG = "NavigationFragment";
+
+    //endregion Constants
+
+    // region Members
+    private GoogleMap mGoogleMap;
+    private LatLng mDummyLatLng = new LatLng(33.6667, 73.1667);
+    private View mCustomMarkerView;
+    private ImageView mMarkerImageView;
+    private String ImageUrl = "https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg";
+
+    // region Members
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        initViews();
+
+        return view;
+    }
+
+    private void initViews() {
+        mCustomMarkerView = ((LayoutInflater) getActivity().getLayoutInflater()).inflate(R.layout.view_custom_marker, null);
+        mMarkerImageView = (ImageView) mCustomMarkerView.findViewById(R.id.profile_image);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady() called with");
+        mGoogleMap = googleMap;
+        MapsInitializer.initialize(getContext());
+        addCustomMarker();
+    }
+
+    private void addCustomMarker() {
+        Log.d(TAG, "addCustomMarker()");
+        if (mGoogleMap == null) {
+            return;
+        }
+
+        // adding a marker on map with image from  drawable
+       /*mGoogleMap.addMarker(new MarkerOptions()
+                .position(mDummyLatLng)
+                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView(mCustomMarkerView, R.drawable.avatar))));*/
+
+        // adding a marker with image from URL using glide image loading library
+        Glide.with(getActivity())
+                .asBitmap()
+                .load(ImageUrl)
+                .fitCenter()
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                        mGoogleMap.addMarker(new MarkerOptions()
+                                .position(mDummyLatLng)
+                                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView(mCustomMarkerView, resource))));
+                        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mDummyLatLng, 13f));
+                    }
+                });
+
+
+    }
+
+    private Bitmap getMarkerBitmapFromView(View view, @DrawableRes int resId) {
+
+        mMarkerImageView.setImageResource(resId);
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache();
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN);
+        Drawable drawable = view.getBackground();
+        if (drawable != null)
+            drawable.draw(canvas);
+        view.draw(canvas);
+        return returnedBitmap;
+    }
+
+    private Bitmap getMarkerBitmapFromView(View view, Bitmap bitmap) {
+
+        mMarkerImageView.setImageBitmap(bitmap);
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache();
+        Bitmap returnedBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(returnedBitmap);
+        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN);
+        Drawable drawable = view.getBackground();
+        if (drawable != null)
+            drawable.draw(canvas);
+        view.draw(canvas);
+        return returnedBitmap;
     }
 
     @Override
