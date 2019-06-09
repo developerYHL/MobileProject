167d61f8c61ea60b399a1328745f03a8fedc4ec8
developerYHL <developer.yhl@gmail.com>
2019-06-09 23:35:35 +0900

    Clustering까지 모두 완료!!!!

---
+++ app/src/main/java/com/example/mobileproject/fragments/BaseDemoActivity.java
@@ -0,0 +1,56 @@
+package com.example.mobileproject.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mobileproject.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public abstract class BaseDemoActivity extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        setUpMap();
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setUpMap();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        if (mMap != null) {
+            return;
+        }
+        mMap = map;
+        startDemo();
+    }
+
+    private void setUpMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Run the demo-specific code.
+     */
+    protected abstract void startDemo();
+
+    protected GoogleMap getMap() {
+        return mMap;
+    }
+}
--- app/src/main/java/com/example/mobileproject/fragments/NavigationFragment.java
+++ app/src/main/java/com/example/mobileproject/fragments/NavigationFragment.java
@@ -1,252 +1,192 @@
 package com.example.mobileproject.fragments;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.support.annotation.DrawableRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.transition.Transition;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import com.example.mobileproject.Activity.MainActivity;
-import com.example.mobileproject.Adapter.RecyclerAdapter;
-import com.example.mobileproject.R;
-import com.example.mobileproject.model.DetailItem;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.maps.android.clustering.ClusterManager;
-
-import android.support.v4.app.FragmentManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.mobileproject.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.Cluster;
+import com.google.maps.android.clustering.ClusterItem;
+import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+import com.google.maps.android.ui.IconGenerator;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-import static android.support.v4.content.ContextCompat.getSystemService;
+public class NavigationFragment extends BaseDemoActivity implements ClusterManager.OnClusterClickListener<Person>, ClusterManager.OnClusterInfoWindowClickListener<Person>, ClusterManager.OnClusterItemClickListener<Person>, ClusterManager.OnClusterItemInfoWindowClickListener<Person>  {
+    private ClusterManager<Person> mClusterManager;
+    private Random mRandom = new Random(1984);
 
-public class NavigationFragment extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    /**
+     * Draws profile photos inside markers (using IconGenerator).
+     * When there are multiple people in the cluster, draw multiple photos (using MultiDrawable).
+     */
+    private class PersonRenderer extends DefaultClusterRenderer<Person> {
+        private final IconGenerator mIconGenerator = new IconGenerator(getActivity());
+        private final IconGenerator mClusterIconGenerator = new IconGenerator(getActivity());
+        private final ImageView mImageView;
+        private final ImageView mClusterImageView;
+        private final int mDimension;
 
-    /*private  static  final int REQUEST_CODE_PERMISSIONS = 1000;
-    private GoogleMap mMap;
-    private FusedLocationProviderClient mFusedLocationClient;
+        public PersonRenderer() {
+            super(getActivity(), getMap(), mClusterManager);
 
-    private ClusterManager<MyItem> mClusterManager;
-    
-    public NavigationFragment() {
-        // Required empty public constructor
+            View multiProfile = getLayoutInflater().inflate(R.layout.multi_profile, null);
+            mClusterIconGenerator.setContentView(multiProfile);
+            mClusterImageView = (ImageView) multiProfile.findViewById(R.id.image);
+
+            mImageView = new ImageView(getActivity());
+            mDimension = (int) getResources().getDimension(R.dimen.custom_profile_image);
+            mImageView.setLayoutParams(new ViewGroup.LayoutParams(mDimension, mDimension));
+            int padding = (int) getResources().getDimension(R.dimen.custom_profile_padding);
+            mImageView.setPadding(padding, padding, padding, padding);
+            mIconGenerator.setContentView(mImageView);
+        }
+
+        @Override
+        protected void onBeforeClusterItemRendered(Person person, MarkerOptions markerOptions) {
+            // Draw a single person.
+            // Set the info window to show their name.
+            mImageView.setImageResource(person.profilePhoto);
+            Bitmap icon = mIconGenerator.makeIcon();
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon)).title(person.name);
+        }
+
+        @Override
+        protected void onBeforeClusterRendered(Cluster<Person> cluster, MarkerOptions markerOptions) {
+            // Draw multiple people.
+            // Note: this method runs on the UI thread. Don't spend too much time in here (like in this example).
+            List<Drawable> profilePhotos = new ArrayList<Drawable>(Math.min(4, cluster.getSize()));
+            int width = mDimension;
+            int height = mDimension;
+
+            for (Person p : cluster.getItems()) {
+                // Draw 4 at most.
+                if (profilePhotos.size() == 4) break;
+                Drawable drawable = getResources().getDrawable(p.profilePhoto);
+                drawable.setBounds(0, 0, width, height);
+                profilePhotos.add(drawable);
+            }
+            MultiDrawable multiDrawable = new MultiDrawable(profilePhotos);
+            multiDrawable.setBounds(0, 0, width, height);
+
+            mClusterImageView.setImageDrawable(multiDrawable);
+            Bitmap icon = mClusterIconGenerator.makeIcon(String.valueOf(cluster.getSize()));
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon));
+        }
+
+        @Override
+        protected boolean shouldRenderAsCluster(Cluster cluster) {
+            // Always render clusters.
+            return cluster.getSize() > 1;
+        }
     }
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
+    public boolean onClusterClick(Cluster<Person> cluster) {
+        // Show a toast with some info when the cluster is clicked.
+        String firstName = cluster.getItems().iterator().next().name;
+        Toast.makeText(getActivity(), cluster.getSize() + " (including " + firstName + ")", Toast.LENGTH_SHORT).show();
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        // Zoom in the cluster. Need to create LatLngBounds and including all the cluster items
+        // inside of bounds, then animate to center of the bounds.
 
-        return view;
+        // Create the builder to collect all essential cluster items for the bounds.
+        LatLngBounds.Builder builder = LatLngBounds.builder();
+        for (ClusterItem item : cluster.getItems()) {
+            builder.include(item.getPosition());
+        }
+        // Get the LatLngBounds
+        final LatLngBounds bounds = builder.build();
+
+        // Animate camera to the bounds
+        try {
+            getMap().animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onClusterInfoWindowClick(Cluster<Person> cluster) {
+        // Does nothing, but you could go to a list of the users.
+    }
+
+    @Override
+    public boolean onClusterItemClick(Person item) {
+        // Does nothing, but you could go into the user's profile page, for example.
+        return false;
+    }
+
+    @Override
+    public void onClusterItemInfoWindowClick(Person item) {
+        // Does nothing, but you could go into the user's profile page, for example.
+    }
+
+    @Override
+    protected void startDemo() {
+        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 9.5f));
+
+        mClusterManager = new ClusterManager<Person>(getActivity(), getMap());
+        mClusterManager.setRenderer(new PersonRenderer());
+        getMap().setOnCameraIdleListener(mClusterManager);
+        getMap().setOnMarkerClickListener(mClusterManager);
+        getMap().setOnInfoWindowClickListener(mClusterManager);
+        mClusterManager.setOnClusterClickListener(this);
+        mClusterManager.setOnClusterInfoWindowClickListener(this);
+        mClusterManager.setOnClusterItemClickListener(this);
+        mClusterManager.setOnClusterItemInfoWindowClickListener(this);
+
+        addItems();
+        mClusterManager.cluster();
     }
 
     private void addItems() {
+        // http://www.flickr.com/photos/sdasmarchives/5036248203/
+        mClusterManager.addItem(new Person(position(), "Walter", R.drawable.walter));
 
-        // Set some lat/lng coordinates to start with.
-        double lat = 51.5145160;
-        double lng = -0.1270060;
+        // http://www.flickr.com/photos/usnationalarchives/4726917149/
+        mClusterManager.addItem(new Person(position(), "Gran", R.drawable.gran));
 
-        // Add ten cluster items in close proximity, for purposes of this example.
-        for (int i = 0; i < 10; i++) {
-            double offset = i / 60d;
-            lat = lat + offset;
-            lng = lng + offset;
-            MyItem offsetItem = new MyItem(lat, lng);
-            mClusterManager.addItem(offsetItem);
-        }
+        // http://www.flickr.com/photos/nypl/3111525394/
+        mClusterManager.addItem(new Person(position(), "Ruth", R.drawable.ruth));
+
+        // http://www.flickr.com/photos/smithsonian/2887433330/
+        mClusterManager.addItem(new Person(position(), "Stefan", R.drawable.stefan));
+
+        // http://www.flickr.com/photos/library_of_congress/2179915182/
+        mClusterManager.addItem(new Person(position(), "Mechanic", R.drawable.mechanic));
+
+        // http://www.flickr.com/photos/nationalmediamuseum/7893552556/
+        mClusterManager.addItem(new Person(position(), "Yeats", R.drawable.yeats));
+
+        // http://www.flickr.com/photos/sdasmarchives/5036231225/
+        mClusterManager.addItem(new Person(position(), "John", R.drawable.john));
+
+        // http://www.flickr.com/photos/anmm_thecommons/7694202096/
+        mClusterManager.addItem(new Person(position(), "Trevor the Turtle", R.drawable.turtle));
+
+        // http://www.flickr.com/photos/usnationalarchives/4726892651/
+        mClusterManager.addItem(new Person(position(), "Teach", R.drawable.teacher));
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(51.86, 0.22)));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(7.0f));
-
-        mMap.setOnInfoWindowClickListener((marker) -> {
-            Intent intent = new Intent((Intent.ACTION_DIAL));
-            intent.setData(Uri.parse("tel:1212345"));
-            if(intent.resolveActivity(getActivity().getPackageManager()) != null){
-                startActivity(intent);
-            }
-        });
-
-        mClusterManager = new ClusterManager<>(getActivity(), mMap);
-        mMap.setOnCameraIdleListener(mClusterManager);
-        mMap.setOnMarkerClickListener(mClusterManager);
-
-        mClusterManager.setOnClusterItemClickListener(new ClusterManager.OnClusterItemClickListener<MyItem>() {
-            @Override
-            public boolean onClusterItemClick(MyItem myItem) {
-                Toast.makeText(getActivity(), myItem.getPosition().toString(), Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
-        addItems();
-    }*/
-
-    // region Constants
-
-    private static final String TAG = "NavigationFragment";
-
-    //endregion Constants
-
-    // region Members
-    private GoogleMap mGoogleMap;
-    private LatLng mDummyLatLng = new LatLng(33.6667, 73.1667);
-    private View mCustomMarkerView;
-    private ImageView mMarkerImageView;
-    private String ImageUrl = "https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg";
-
-    // region Members
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
-        initViews();
-
-        return view;
+    private LatLng position() {
+        return new LatLng(random(51.6723432, 51.38494009999999), random(0.148271, -0.3514683));
     }
 
-    private void initViews() {
-        mCustomMarkerView = ((LayoutInflater) getActivity().getLayoutInflater()).inflate(R.layout.view_custom_marker, null);
-        mMarkerImageView = (ImageView) mCustomMarkerView.findViewById(R.id.profile_image);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Log.d(TAG, "onMapReady() called with");
-        mGoogleMap = googleMap;
-        MapsInitializer.initialize(getContext());
-        addCustomMarker();
-    }
-
-    private void addCustomMarker() {
-        Log.d(TAG, "addCustomMarker()");
-        if (mGoogleMap == null) {
-            return;
-        }
-
-        // adding a marker on map with image from  drawable
-       /*mGoogleMap.addMarker(new MarkerOptions()
-                .position(mDummyLatLng)
-                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView(mCustomMarkerView, R.drawable.avatar))));*/
-
-        // adding a marker with image from URL using glide image loading library
-        Glide.with(getActivity())
-                .asBitmap()
-                .load(ImageUrl)
-                .fitCenter()
-                .into(new SimpleTarget<Bitmap>() {
-                    @Override
-                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
-                        mGoogleMap.addMarker(new MarkerOptions()
-                                .position(mDummyLatLng)
-                                .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView(mCustomMarkerView, resource))));
-                        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mDummyLatLng, 13f));
-                    }
-                });
-
-
-    }
-
-    private Bitmap getMarkerBitmapFromView(View view, @DrawableRes int resId) {
-
-        mMarkerImageView.setImageResource(resId);
-        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
-        view.buildDrawingCache();
-        Bitmap returnedBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(),
-                Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(returnedBitmap);
-        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN);
-        Drawable drawable = view.getBackground();
-        if (drawable != null)
-            drawable.draw(canvas);
-        view.draw(canvas);
-        return returnedBitmap;
-    }
-
-    private Bitmap getMarkerBitmapFromView(View view, Bitmap bitmap) {
-
-        mMarkerImageView.setImageBitmap(bitmap);
-        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
-        view.buildDrawingCache();
-        Bitmap returnedBitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(),
-                Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(returnedBitmap);
-        canvas.drawColor(Color.WHITE, PorterDuff.Mode.SRC_IN);
-        Drawable drawable = view.getBackground();
-        if (drawable != null)
-            drawable.draw(canvas);
-        view.draw(canvas);
-        return returnedBitmap;
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+    private double random(double min, double max) {
+        return mRandom.nextDouble() * (max - min) + min;
     }
 }
