9e82990a443718e66f61deb3e42858c3798b75f6
csh <tizkzks96@gmail.com>
2019-06-16 23:55:02 +0900

    몇개 수정

--- app/src/main/java/com/example/mobileproject/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/mobileproject/Activity/LoginActivity.kt
@@ -1,34 +1,26 @@
 package com.example.mobileproject.Activity
 
+import android.app.Activity
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import android.widget.EditText
-
 import com.example.mobileproject.R
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.OnFailureListener
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
-
-import java.util.Arrays
-import java.util.HashMap
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
     private val mAuth: FirebaseAuth? = null
-    internal var db: FirebaseFirestore
+    private lateinit var db: FirebaseFirestore
     private var mUser: FirebaseUser? = null
 
     private var context: Context? = null
@@ -46,14 +38,14 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
 
-        findViewById<View>(R.id.email_login).setOnClickListener({ v ->
+        findViewById<View>(R.id.email_login).setOnClickListener {
             startActivityForResult(
                     AuthUI.getInstance()
                             .createSignInIntentBuilder()
                             .setAvailableProviders(providers)
                             .build(),
                     RC_SIGN_IN)
-        })
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -146,6 +138,6 @@ class LoginActivity : AppCompatActivity() {
 
     companion object {
 
-        private val RC_SIGN_IN = 1000
+        private const val RC_SIGN_IN = 1000
     }
 }
