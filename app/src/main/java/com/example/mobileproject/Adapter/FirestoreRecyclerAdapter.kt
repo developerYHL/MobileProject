7b4acc65d4777ba27f60f9acf8611941cfd761d4
csh <tizkzks96@gmail.com>
2019-06-17 00:09:28 +0900

    FirestoreRecyclerAdapter convert java to kotlin

---
+++ app/src/main/java/com/example/mobileproject/Adapter/FirestoreRecyclerAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.mobileproject.Adapter
+
+import android.view.ViewGroup
+
+import com.example.mobileproject.Activity.PostActivity
+import com.example.mobileproject.holder.HomeItemHolder
+import com.example.mobileproject.model.DetailItem
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+
+abstract class FirestoreRecyclerAdapter
+/**
+ * Create a new RecyclerView adapter that listens to a Firestore Query.  See [ ] for configuration options.
+ *
+ * @param options
+ */
+(options: FirestoreRecyclerOptions<DetailItem>) : com.firebase.ui.firestore.FirestoreRecyclerAdapter<DetailItem, HomeItemHolder>(options) {
+
+    abstract override fun onBindViewHolder(holder: HomeItemHolder, position: Int, model: DetailItem)
+
+    abstract override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): HomeItemHolder
+}
